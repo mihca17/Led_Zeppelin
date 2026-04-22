0fa80e53fb29506821e5513a5e214097927547ba
Misha <mihcamihca@mail.ru>
2026-04-22 14:42:05 +0500

    Установили проект и приложение django исправили основные функции

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Params():
     def __init__(self):
-        self.team_id = '183395ba'
+        # self.team_id = '183395ba'
+        self.team_id = 'c216f97a'
--- test.file.py
+++ test.file.py
@@ -3,21 +3,30 @@ from config import Params
 
 parameters = Params()
 team_id =parameters.team_id
-print(team_id)
+
+def register_team():
+    url = f'https://mephi.opentoshi.net/api/v1/team/register'
+    connection = requests.get(url)
+    data = connection.json()
+    return data
 
 def get_data(id):
-    url = f'https://mephi.opentoshi.net/api/v1/team/data?team_id={id}'
+    url = f'https://mephi.opentoshi.net/api/v1/reactor/data?team_id={id}'
+    print(url)
     connection = requests.get(url)
     data = connection.json()
     return data
 
 def create_reactor(id):
-    url = f'https://mephi.opentoshi.net/api/v1/reacor/create_reactor?team_id={id}'
+    url = f'https://mephi.opentoshi.net/api/v1/reactor/create_reactor?team_id={id}'
     connection = requests.post(url)
     data = connection.json()
     return data
 
-print(f'RESULTS:\nteam_id: {team_id}\nget_data(team_id): {get_data(team_id)}\ncreate_reactor(team_id): {create_reactor(team_id)}')
+print(f'RESULTS:'
+      # f'\nteam_id: {team_id}'
+      # f'\ncreate_reactor(team_id): {create_reactor(team_id)}'
+      f'\nget_data(team_id): {get_data(team_id)}')
 
 # url = 'https://mephi.opentoshi.net/api/v1/team/register'
 #
